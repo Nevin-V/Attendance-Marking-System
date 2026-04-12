e7d495d2a51eaa35725f20ced33bfe58e64d2f72
Nevin v skaria <nevinvskaria@gmail.com>
2026-04-12 13:51:08 +0530

    changes made to try deployment

---
+++ backend/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Render.com build script for the Django backend
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
